9bcfac78b85fceb42d02fa2d9024fd6e643e286e
William Nascimento <william.nascimento2010@gmail.com>
2024-11-24 10:26:57 -0300

    adicionado novos graficos dahsboard

---
+++ Solucao.Application/Contracts/SeriesDataDriverOrEquipmentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Solucao.Application.Contracts
+{
+    public class SeriesDataDriverOrEquipmentViewModel
+    {
+        public string Name { get; set; }
+        public int Total { get; set; }
+
+    }
+}
+
